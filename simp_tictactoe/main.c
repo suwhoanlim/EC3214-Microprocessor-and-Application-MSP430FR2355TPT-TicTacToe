0a04df1761985ed77ba1915484c7eaa2667bbecf
Suwhoan Lim <ludence1029@gmail.com>
2023-06-09 11:08:11 +0900

    Code and circuit works fine - no dc motor
    code works, circuit works without dc motor connection

--- simp_tictactoe/main.c
+++ simp_tictactoe/main.c
@@ -5,8 +5,27 @@
 void init_player_switch(); // init player switches
 void init_board_pins(); // init tictactoe led pins
 void init_analogue_sensor(); // init potentiometer
+
+void init_pwm();
+void init_encoder();
+void init_timer();
+
 void test_led_on(); // Test if LED work properly
 void test_led_off(); // Test if LED work properly
+
+int b1 = 0;
+int b2 = 0;
+int b3 = 0;
+int r1 = 0;
+int r2 = 0;
+int r3 = 0;
+int ADC_value = 0;
+int test_b = 0;
+int test_r = 0;
+
+int EncoderCount = 0;
+float Duty = 0.2;
+
 
 /**
  * main.c
@@ -18,12 +37,17 @@ int main(void)
 	init_player_switch();
 	init_board_pins();
 	init_analogue_sensor();
+	init_pwm();
+	init_encoder();
+	init_timer();
 
-	//turn_led_on();
-	//turn_led_off();
 
     __enable_interrupt();
     PM5CTL0 &= ~LOCKLPM5;
+
+    //test_led_on();
+    test_led_off();
+
 
 	while(1) {
         /*
@@ -68,6 +92,8 @@ __interrupt void ISR_turn_LED_off() { // P6.0, 6.1, 6.2 // P2.0, 2.2, 4.0
     /*
      * Red LED
      */
+    test_b ++;
+
     P6OUT &= ~BIT0;
     P6OUT &= ~BIT1;
     P6OUT &= ~BIT2;
@@ -82,7 +108,7 @@ __interrupt void ISR_turn_LED_off() { // P6.0, 6.1, 6.2 // P2.0, 2.2, 4.0
 
 #pragma vector = PORT3_VECTOR
 __interrupt void ISR_player1_switch_pressed() { // P6.0, 6.1, 6.2
-    int ADC_value;
+    //int ADC_value;
 
     ADCCTL0 |= ADCENC | ADCSC; // start reading ADC value
     while((ADCIFG & ADCIFG0) == 0); // wait untill conversion is finished
@@ -96,15 +122,25 @@ __interrupt void ISR_player1_switch_pressed() { // P6.0, 6.1, 6.2
      * If LED is on, turn it off
      */
     if(ADC_value < 1300) {
-        if(P6OUT & BIT0) { // check if LED is on
+        P6OUT ^=BIT0; // if LED is on, turn it off
+    }
+    else if ((1300 <= ADC_value) && (ADC_value < 2600)){
+        P6OUT ^= BIT1;
+    }
+    else if(2600 <= ADC_value) {
+        P6OUT ^= BIT2;
+    }
+    /*
+    if(ADC_value < 1300) {
+        if(P6OUT & BIT0 != 0) { // check if LED is on
             P6OUT &= ~BIT0; // if LED is on, turn it off
         }
         else {
             P6OUT |= BIT0; // if LED is off, turn it on
         }
     }
-    else if ((1300 <= ADC_value) && (2600 < ADC_value)){
-        if(P6OUT & BIT1) {
+    else if ((1300 <= ADC_value) && (ADC_value < 2600)){
+        if(P6OUT & BIT1 != 0) {
             P6OUT &= ~BIT1;
         }
         else {
@@ -112,27 +148,47 @@ __interrupt void ISR_player1_switch_pressed() { // P6.0, 6.1, 6.2
         }
     }
     else if(2600 <= ADC_value) {
-        if(P6OUT & BIT2) {
+        if(P6OUT & BIT2 != 0) {
             P6OUT &= ~BIT2;
         }
         else {
             P6OUT |= BIT2;
         }
     }
+    */
+    P3IFG &= ~BIT0;
 }
 
 #pragma vector = PORT2_VECTOR
 __interrupt void ISR_player2_switch_pressed() { // P2.0 2.2 4.0
-    int ADC_value;
+    //int ADC_value;
 
     /*
      * if all three LED are on, turn them off and end ISR
      */
+
+    /*
     if((P6OUT & BIT0) && (P2OUT & BIT2) && (P4OUT & BIT0)) {
-        P2OUT &= ~BIT0;
-        P2OUT &= ~BIT2;
-        P4OUT &= ~BIT0;
+        test ++;
         return;
+    }
+    */
+    test_b++;
+
+    if(b1 == 1) {
+        if(b2 == 1) {
+            if(b3 == 1){
+                P2OUT &= ~BIT0;
+                P2OUT &= ~BIT2;
+                P4OUT &= ~BIT0;
+                b1 = 0;
+                b2 = 0;
+                b3 = 0;
+
+                P2IFG &= ~BIT5;
+                return;
+            }
+        }
     }
 
     /*
@@ -145,15 +201,72 @@ __interrupt void ISR_player2_switch_pressed() { // P2.0 2.2 4.0
 
     if(ADC_value < 1300) {
         P2OUT |= BIT0;
+        b1 = 1;
     }
-    else if ((1300 <= ADC_value) && (2600 < ADC_value)){
+    else if ((1300 <= ADC_value) && (ADC_value < 2600)){
         P2OUT |= BIT2;
+        b2 = 1;
     }
     else if(2600 <= ADC_value) {
         P4OUT |= BIT0;
+        b3 = 1;
     }
+    int i = 0;
+    for(i = 0x0FFFF; i>0; i--) {} //delay
 
+    P2IFG &= ~BIT5;
 }
+
+/* ISR for PWM (Pulse generation) */
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void ISR_TB0_CCR0(void)
+{
+    P5OUT |= BIT2;
+    TB0CCTL0 &= ~CCIFG;
+}
+#pragma vector = TIMER0_B1_VECTOR
+__interrupt void ISR_TB0_CCR1(void)
+{
+    P5OUT &= ~BIT2;
+    TB0CCTL1 &= ~CCIFG;
+}
+
+/* ISR for Timer (Speed control) */
+#pragma vector = TIMER1_B0_VECTOR
+__interrupt void ISR_TB1_CCR0(void) {
+    //SpeedMeas = EncoderCount*0.625;         // Calculate the motor speed from the pulse count
+
+    TB0CCR1 = (int)(999*Duty);      // Determine TB0 CCR1
+    EncoderCount = 0;               // Reset the pulse count
+    TB1CCTL0 &= ~CCIFG;             // Clear TB1 CCR0 Flag
+}
+
+
+#pragma vector = TIMER2_B0_VECTOR   //TB2CCR0
+__interrupt void ISR_TB2_CCR0(void)
+{
+    //SpeedCmd = 30;
+    Duty = 0.3;
+    TB2CCTL0 &= ~CCIFG;
+}
+
+#pragma vector = TIMER2_B1_VECTOR   //TB2CCR1
+__interrupt void ISR_TB2_CCR(void)
+{
+    if (TB2CCTL1 & CCIFG){
+        //SpeedCmd = 60;
+        Duty = 0.5;
+        TB2CCTL1 &= ~CCIFG;}
+    else if(TB2CCTL2 & CCIFG){
+        //SpeedCmd = 120;
+        Duty = 0.9;
+        TB2CCTL2 &= ~CCIFG;}
+}
+
+
+/*
+ * Below are Test codes and init codes
+ */
 
 void test_led_on() {
     /*
@@ -163,15 +276,15 @@ void test_led_on() {
     /*
      * Red LED
      */
-    P6OUT |= BIT0;
+    P6OUT &= ~BIT0;
     P6OUT |= BIT1;
-    P6OUT |= BIT2;
+    P6OUT &= ~BIT2;
 
     /*
      * Blue LED
      */
     P2OUT |= BIT0;
-    P2OUT |= BIT2;
+    P2OUT &= ~BIT2;
     P4OUT |= BIT0;
 }
 
@@ -267,4 +380,53 @@ void init_analogue_sensor() {
     ADCMCTL0 |= ADCINCH_2; // Use channel A2(P1.2)
 
     //ADCIE |= ADCIE0; // comment out the interrupt flag. We want to manually read ADC value
+}
+
+void init_pwm(){
+    P5DIR |= BIT2;              // Configure P1.6 as output
+    P5OUT |= BIT2;              // Set the initial value as 1
+
+    TB0CTL |= TBCLR;            // Clear TB0
+    TB0CTL |= TBSSEL__SMCLK;    // Source = SMCLK (1Mhz)
+    TB0CTL |= MC__UP;           // Mode = Up
+    TB0CCR0 = 999;              // CCR0 = 999 -> PWM Frequency = 1MHz/(999 + 1) = 1 kHz
+    TB0CCR1 = 0;                // CCR1 = 0, initialization
+
+    TB0CCTL0 |= CCIE;           // Enable TB0 CCR0 IRQ
+    TB0CCTL0 &= ~CCIFG;         // Clear TB0 CCR0 Flag
+    TB0CCTL1 |= CCIE;           // Enable TB0 CCR1 IRQ
+    TB0CCTL1 &= ~CCIFG;         // Clear TB0 CCR1 Flag
+}
+
+void init_encoder(void){
+    P5DIR &= ~BIT0;             // Configure P.1.1(Encoder) as input
+    P5IES &= ~BIT0;             // Configure IRQ Sensitivity H-to-L
+
+    P5IFG &= ~BIT0;             // Clear P1.1(Encoder) IRQ Flag 5.0
+    P5IE |= BIT0;
+}
+
+void init_timer(void){
+    TB1CTL |= TBCLR;            // Clear TB1
+    TB1CTL |= TBSSEL__ACLK;     // Source = ACLK (32.768kHz)
+    TB1CTL |= MC__UP;           // Mode = Up
+    TB1CCR0 = 3276;             // CCR0 = 3276 -> Control Frequency = 32.768kHz/(3276 + 1) = 10 Hz
+
+    TB1CCTL0 |= CCIE;           // Enable TB1 CCR0 IRQ
+    TB1CCTL0 &= ~CCIFG;         // Clear TB1 CCR0 Flag
+
+    TB2CTL |= TBCLR;            // Clear TB2
+    TB2CTL |= TBSSEL__ACLK;     // Source = ACLK (32.768kHz)
+    TB2EX0 |= TBIDEX_4;         // Divide by 5 by using IDEX -> 1 cycle = 10s
+    TB2CTL |= MC__UP;            // Mode = UP
+    TB2CCR0 = 65530;            // CCR0 = 65535 -> Control Frequency 32.768kHz/5 * 65535 ~= 10s
+    TB2CCR1 = 21845;            // CCR1 = 43691 -> Control Frequency 32.768kHz/5 * 43691 ~= 3s
+    TB2CCR2 = 43691;
+
+    TB2CCTL0 |= CCIE;           // Enable TB2 CCR0 IRQ
+    TB2CCTL0 &= ~CCIFG;         // Clear TB2 CCR0 Flag
+    TB2CCTL1 |= CCIE;           // Enable TB2 CCR1 IRQ
+    TB2CCTL1 &= ~CCIFG;         // Clear TB2 CCR1 Flag
+    TB2CCTL2 |= CCIE;           // Enable TB2 CCR2 IRQ
+    TB2CCTL2 &= ~CCIFG;         // Clear TB2 CCR2 Flag
 }
